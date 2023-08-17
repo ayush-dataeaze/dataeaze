157f67fbb7a44f398376f1853f23f7276b17e3c8
ayush_7 <ayush.shukla@dataeaze.io>
2023-08-17 16:59:10 +0530

    added llamma

--- backend/venv/app.py
+++ backend/venv/app.py
@@ -1,40 +1,38 @@
 from flask import Flask, request, jsonify
+import transformers
+from torch import cuda, bfloat16
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
 app = Flask(__name__)
 
-def initialize_custom_model():
-    print("Please wait while I load onto your GPU memory...")
-    model_id = 'meta-llama/Llama-2-7b-chat-hf'
-    token = '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'
-    
-    model_config = transformers.AutoConfig.from_pretrained(
-        model_id,
-        use_auth_token=token
-    )
-    
-    bnb_config = transformers.BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type='nf4',
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=bfloat16
-    )
-    
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_id,
-        trust_remote_code=True,
-        config=model_config,
-        device_map='auto',
-        quantization_config=bnb_config,
-        use_auth_token=token
-    )
-    
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        model_id,
-        use_auth_token=token
-    )
-    
-    print("Model loaded onto your GPU memory")
-    return model, tokenizer
+def initialize_custom_model(): 
+        print("Please wait while I load onto your gpu memory")
+        model_id = 'meta-llama/Llama-2-7b-chat-hf'
+        token = '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'
+        model_config = transformers.AutoConfig.from_pretrained(
+                                    model_id,
+                                            use_auth_token=token
+                                                )
+        bnb_config = transformers.BitsAndBytesConfig(
+                        load_in_4bit=True,
+                                bnb_4bit_quant_type='nf4',
+                                        bnb_4bit_use_double_quant=True,
+                                                bnb_4bit_compute_dtype=bfloat16
+                                                    )
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+                        model_id,
+                                trust_remote_code=True,
+                                        config=model_config,
+                                                device_map='auto',
+                                                        quantization_config=bnb_config,
+                                                                use_auth_token=token
+                                                                    )
+        tokenizer = transformers.AutoTokenizer.from_pretrained(
+                        model_id,
+                                use_auth_token=token
+                                    )
+        print("Model loaded onto your gpu memory")
+        return model, tokenizer
 
 # Initialize the model and tokenizer only once
 model, tokenizer = initialize_custom_model()
